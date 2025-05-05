3d9c6accee016ae983f9fb98b67aa74b5a3d1377
Daniel Moreira <128258066+diogo735@users.noreply.github.com>
2025-05-05 10:28:52 +0100

    bug

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.3.3',
-        'version' => '10.3.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f05cfc275931b42abf24daf73a3f28ed84dcd17',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.3.3',
-            'version' => '10.3.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f05cfc275931b42abf24daf73a3f28ed84dcd17',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
