0e9a1fa6a5fcb8b21243abaac3b676304cd79ef2
Daniel Moreira <128258066+diogo735@users.noreply.github.com>
2025-05-27 10:40:46 +0100

    nova bd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,21 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Agenda de comandos
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        if (file_exists(base_path('routes/console.php'))) {
+            require base_path('routes/console.php');
+        }
     }
 }
